e6d7c46a145f6262174f7af83f5ae2300d355dcb
DavidAlonso858 <davidmadrid858@gmail.com>
2024-10-24 14:11:57 +0200

    tomcat prueba

--- TomcatPrueba/src/main/java/com/example/demo/HelloServlet.java
+++ TomcatPrueba/src/main/java/com/example/demo/HelloServlet.java
@@ -5,23 +5,29 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "hello", value = "/nombre-server")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello User! ";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // ponemos doPost por que el formulario usamos post
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        String apellido = request.getParameter("apellidoUsuario");
+        String dni = request.getParameter("dniUsuario");
+
+        message = "<h1>" + message + request.getParameter("nombreUsuario") + " " + apellido + " con DNI " + dni + "</h1>";
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println(message);
         out.println("</body></html>");
     }
+
 
     public void destroy() {
     }
