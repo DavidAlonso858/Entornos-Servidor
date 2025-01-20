db583deee6bc69971f11a4853707f2ba45581a20
DavidAlonso858 <davidmadrid858@gmail.com>
2025-01-20 14:32:17 +0100

    principio de implementacion del service, controller y cambio de idioma en los messages

---
+++ SPRING/spring_boot_web_mvc_jdbc_ventas-master/src/main/java/org/iesbelen/controlador/ComercialController.java
@@ -0,0 +1,28 @@
+package org.iesbelen.controlador;
+
+import org.iesbelen.dao.ComercialDAO;
+import org.iesbelen.modelo.Comercial;
+import org.iesbelen.service.ComercialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ComercialController {
+
+    @Autowired
+    private ComercialService comercialService;
+
+    @GetMapping("/comercial")
+    public String listar(Model model) {
+        List<Comercial> listaComercial = comercialService.listAll();
+
+        model.addAttribute("listaComercial", listaComercial);
+
+        return "comerciales";
+    }
+
+}
