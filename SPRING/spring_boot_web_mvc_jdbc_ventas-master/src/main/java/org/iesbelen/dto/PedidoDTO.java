ff5db4afd9de8d5b753a2d0ce35c672b45606593
DavidAlonso858 <davidmadrid858@gmail.com>
2025-01-30 13:54:48 +0100

    anotacion de data sustitutye get y setter, anotacion de arg o noarg sustituye los constructores

--- SPRING/spring_boot_web_mvc_jdbc_ventas-master/src/main/java/org/iesbelen/dto/PedidoDTO.java
+++ SPRING/spring_boot_web_mvc_jdbc_ventas-master/src/main/java/org/iesbelen/dto/PedidoDTO.java
@@ -2,12 +2,14 @@ package org.iesbelen.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.iesbelen.modelo.Pedido;
 
 import java.util.Date;
 
 @Data // reemplaza los getter y setter tambien vale la anotacion @getter y @setter
-@AllArgsConstructor
+@AllArgsConstructor // devuelve un constructor con todos los argumentos
+@NoArgsConstructor // devuelve un constructor sin argumentos
 // lombok no incorpora bien herencia
 // por eso mejor copiar los atributos y no poner extends
 public class PedidoDTO {
